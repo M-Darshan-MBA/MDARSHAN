980e04b04ebccea011109588a7883db511dc0187
M-Darshan-MBA <ddarshan861@gmail.com>
2026-05-28 23:27:15 +0530

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,16 @@
+@echo off
+color 0A
+title Darshan Setup
+
+echo ====================================
+echo      WELCOME DARSHAN
+echo ====================================
+
+echo Download successful...
+timeout /t 2 >nul
+
+echo Installing components...
+timeout /t 2 >nul
+
+echo Setup completed successfully.
+pause
